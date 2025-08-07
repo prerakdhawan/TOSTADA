c97b27b5bccd4795e33ed82f3c5e4734b06d5f9e
Prerak Dhawan <prerak.dhawan@physik.uni-halle.de>
2025-08-07 06:23:01 +0200

    Added setup files for distribution

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tostada',
+    version='0.1.0',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    include_package_data=True,
+    package_data={
+        'tostada': ['util/*.csv', 'util/*.txt'],
+    },
+    description='Toolkit for spatially tailored disordered arrangements',
+    author='Prerak Dhawan',
+)
